ce4a5c805405647da03c9d48be3ccafe44956cc2
azuazu3939 <nnokan0922@gmail.com>
2025-09-05 19:19:49 +0900

    リファクタリング 文字の大きさ

--- src/main/java/dev/felnull/storagegui/commands/GUIReload.java
+++ src/main/java/dev/felnull/storagegui/commands/GUIReload.java
@@ -10,8 +10,10 @@ public class GUIReload implements CommandExecutor {
 
     private final StorageGUI plugin;
 
-    private static String PAGE_BACK, RETURN_MAIN, BUY_STORAGE, BUY_FAIL, ADD_VIEW_POS, SUB_VIEW_POS, SCROLL_POS;
+    private static String PAGE_BACK, RETURN_MAIN, BUY_STORAGE, BUY_FAIL, SCROLL_POS;
     private static String SYMBOL;
+    private static String ADD_VIEW_POS = StorageGUI.getInstance().getConfig().getString("messages.add-view-pos", "§r§aスロットPosを上げる");
+    private static String SUB_VIEW_POS = StorageGUI.getInstance().getConfig().getString("messages.sub-view-pos", "§r§bスロットPosを下げる");
 
     public GUIReload(StorageGUI storageGUI) {
         this.plugin = storageGUI;
@@ -26,8 +28,8 @@ public class GUIReload implements CommandExecutor {
         RETURN_MAIN = plugin.getConfig().getString("messages.return-main", "§r§c§lキャンセルする");
         BUY_STORAGE = plugin.getConfig().getString("messages.buy-storage", "§r§e§l%value%" + SYMBOL + "でストレージを購入する!");
         BUY_FAIL = plugin.getConfig().getString("messages.buy-fail", "§r§c§lお金が %lack%" + SYMBOL + " 足りません! 現在: %value%" + SYMBOL);
-        ADD_VIEW_POS = plugin.getConfig().getString("messages.add-view-pos", "§r§a§lスロットPosを上げる");
-        SUB_VIEW_POS = plugin.getConfig().getString("messages.sub-view-pos", "§r§b§lスロットPosを下げる");
+        ADD_VIEW_POS = plugin.getConfig().getString("messages.add-view-pos", "§r§aスロットPosを上げる");
+        SUB_VIEW_POS = plugin.getConfig().getString("messages.sub-view-pos", "§r§bスロットPosを下げる");
         SCROLL_POS = plugin.getConfig().getString("messages.scroll-pos", "§r§6§l下にスクロール");
         return true;
     }
