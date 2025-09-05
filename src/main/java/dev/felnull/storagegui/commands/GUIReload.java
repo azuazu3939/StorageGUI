bcdc8e5792c7be525456c2d6bd006d9efa55c722
azuazu3939 <nnokan0922@gmail.com>
2025-09-05 19:47:41 +0900

    リファクタリング 正しい実装

--- src/main/java/dev/felnull/storagegui/commands/GUIReload.java
+++ src/main/java/dev/felnull/storagegui/commands/GUIReload.java
@@ -1,19 +1,26 @@
 package dev.felnull.storagegui.commands;
 
 import dev.felnull.storagegui.StorageGUI;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 public class GUIReload implements CommandExecutor {
 
     private final StorageGUI plugin;
+    private static final FileConfiguration config = StorageGUI.getInstance().getConfig();
 
-    private static String PAGE_BACK, RETURN_MAIN, BUY_STORAGE, BUY_FAIL, SCROLL_POS;
-    private static String SYMBOL;
-    private static String ADD_VIEW_POS = StorageGUI.getInstance().getConfig().getString("messages.add-view-pos", "§r§aスロットPosを上げる");
-    private static String SUB_VIEW_POS = StorageGUI.getInstance().getConfig().getString("messages.sub-view-pos", "§r§bスロットPosを下げる");
+    private static String SYMBOL = config.getString("symbol", "$");
+    private static String ADD_VIEW_POS = config.getString("messages.add-view-pos", "§r§aスロットPosを上げる");
+    private static String SUB_VIEW_POS = config.getString("messages.sub-view-pos", "§r§bスロットPosを下げる");
+    private static String RETURN_MAIN = config.getString("messages.return-main", "§r§c§lキャンセルする");
+    private static String PAGE_BACK = config.getString("messages.page-back", "§r§6§lストレージオプションに戻る");
+    private static String BUY_STORAGE = config.getString("messages.buy-storage", "§r§e§l%value%" + SYMBOL + "でストレージを購入する!");
+    private static String BUY_FAIL = config.getString("messages.buy-fail", "§r§c§lお金が %lack%" + SYMBOL + " 足りません! 現在: %value%" + SYMBOL);
+    private static String SCROLL_POS = config.getString("messages.scroll-pos", "§r§6§l下にスクロール");
 
     public GUIReload(StorageGUI storageGUI) {
         this.plugin = storageGUI;
@@ -22,7 +29,7 @@ public class GUIReload implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
         plugin.reloadConfig();
-        SYMBOL = plugin.getConfig().getString("symbol");
+        SYMBOL = plugin.getConfig().getString("symbol", "$");
 
         PAGE_BACK = plugin.getConfig().getString("messages.page-back", "§r§6§lストレージオプションに戻る");
         RETURN_MAIN = plugin.getConfig().getString("messages.return-main", "§r§c§lキャンセルする");
@@ -31,6 +38,8 @@ public class GUIReload implements CommandExecutor {
         ADD_VIEW_POS = plugin.getConfig().getString("messages.add-view-pos", "§r§aスロットPosを上げる");
         SUB_VIEW_POS = plugin.getConfig().getString("messages.sub-view-pos", "§r§bスロットPosを下げる");
         SCROLL_POS = plugin.getConfig().getString("messages.scroll-pos", "§r§6§l下にスクロール");
+
+        commandSender.sendMessage(Component.text("§aリロードが完了しました。"));
         return true;
     }
 
